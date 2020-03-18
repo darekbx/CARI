9b231f875960f6f23a8b74f0c12e9a930bfca8d6
DarekMac <darek.baranczuk@gmail.com>
2020-03-18 08:43:57 +0100

    CARI Fixed problem when port is already in use

--- cari-sdk/src/main/java/com/darekbx/cari/sdk/internal/communication/SocketCommunication.kt
+++ cari-sdk/src/main/java/com/darekbx/cari/sdk/internal/communication/SocketCommunication.kt
@@ -1,9 +1,12 @@
 package com.darekbx.cari.sdk.internal.communication
 
 import android.os.AsyncTask
+import android.util.Log
+import com.darekbx.cari.BuildConfig
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
+import java.net.BindException
 import java.net.ServerSocket
 
 internal class SocketCommunication(val port: Int) {
@@ -14,25 +17,34 @@ internal class SocketCommunication(val port: Int) {
 
     fun start() {
         AsyncTask.execute {
-            val serverSocket = ServerSocket(port)
-            while (true) {
+            try {
+                val serverSocket = ServerSocket(port)
+                while (true) {
 
-                val socketRead = serverSocket.accept()
+                    val socketRead = serverSocket.accept()
 
-                val input = socketRead.getInputStream()
-                val outputWriter = PrintWriter(socketRead.getOutputStream(), true)
-                val inputReader = InputStreamReader(input, CHARSET)
-                val bufferedReader = BufferedReader(inputReader)
+                    val input = socketRead.getInputStream()
+                    val outputWriter = PrintWriter(socketRead.getOutputStream(), true)
+                    val inputReader = InputStreamReader(input, CHARSET)
+                    val bufferedReader = BufferedReader(inputReader)
 
-                val receivedData = bufferedReader.readLine()
-                val dataToWrite = callback?.invoke(receivedData)
-                outputWriter.println(dataToWrite)
+                    val receivedData = bufferedReader.readLine()
+                    val dataToWrite = callback?.invoke(receivedData)
+                    outputWriter.println(dataToWrite)
 
-                inputReader.close()
-                bufferedReader.close()
-                outputWriter.close()
+                    inputReader.close()
+                    bufferedReader.close()
+                    outputWriter.close()
 
-                socketRead.close()
+                    socketRead.close()
+                }
+            } catch (e: Exception) {
+                if (e is BindException) {
+                    Log.e("CARI", "There is a problem with communication: ${e.message}")
+                }
+                if (BuildConfig.DEBUG) {
+                    e.printStackTrace()
+                }
             }
         }
     }
